a85165f87ec641d37800207734f90bf3152c5d89
Jgarcia93 <juan17241@gmail.com>
2023-12-07 12:10:58 -0800

    Changed ListView Quadrant text

--- Screens/ListScreen.js
+++ Screens/ListScreen.js
@@ -12,18 +12,18 @@ export default function ListScreen() {
           <Text style={styles.recName}>Receptacle ID: {rec.rid}</Text>
           <View style={styles.row}>
             <View style={[styles.col, rec.fillq1 < 25 ? styles.greenFull : rec.fillq1 > 25 && rec.fillq1 < 70 ? styles.orangeFull : styles.redFull]}>
-              <Text style={styles.recFill}>Quadrant 1: {rec.fillq1}% </Text>
+              <Text style={styles.recFill}>{rec.fillq1}% </Text>
             </View>
             <View style={[styles.col, rec.fillq2 < 25 ? styles.greenFull : rec.fillq2 > 25 && rec.fillq2 < 70 ? styles.orangeFull : styles.redFull]}>
-              <Text style={styles.recFill}>Quadrant 2: {rec.fillq2}%</Text>
+              <Text style={styles.recFill}>{rec.fillq2}%</Text>
             </View>
           </View>
           <View style={styles.row}>
             <View style={[styles.col, rec.fillq3 < 25 ? styles.greenFull : rec.fillq3 > 25 && rec.fillq3 < 70 ? styles.orangeFull : styles.redFull]}>
-              <Text style={styles.recFill}>Quadrant 3: {rec.fillq3}% </Text>
+              <Text style={styles.recFill}>{rec.fillq3}% </Text>
             </View>
             <View style={[styles.col, rec.fillq4 < 25 ? styles.greenFull : rec.fillq4 > 25 && rec.fillq4 < 70 ? styles.orangeFull : styles.redFull]}>
-              <Text style={styles.recFill}>Quadrant 4: {rec.fillq4}%</Text>
+              <Text style={styles.recFill}>{rec.fillq4}%</Text>
             </View>
           </View>
           <Text style={{fontSize: 20}}>Average Fill: {(rec.fillq1+rec.fillq2+rec.fillq3+rec.fillq4)/4}%</Text>
